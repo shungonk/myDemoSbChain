fdae45c052ce25ef8ece3209cd304d99f2c97f72
Shungo Nakanishi <snakanishi@ShungonoMacBook-Pro.local>
2021-02-27 12:36:31 +0900

    change validation order

--- src/main/java/com/myexample/ApplicationServer.java
+++ src/main/java/com/myexample/ApplicationServer.java
@@ -115,12 +115,12 @@ public class ApplicationServer {
                 System.out.println(req.marshalJsonPrettyPrinting());
 
                 Result result;
-                if (!req.validateValue())
-                    result = Result.NOT_POSITIVE_VALUE;
-                else if (!req.validateTransactionRequest())
+                if (!req.validateTransactionRequest())
                     result = Result.MISSING_FIELDS;
                 else if (!req.verifySignature())
                     result = Result.INVALID_SIGNATURE;
+                else if (!req.validateValue())
+                    result = Result.NOT_POSITIVE_VALUE;
                 else if (!req.veritfyAddress())
                     result = Result.INCONSISTENT_ADDRESS;
                 else
